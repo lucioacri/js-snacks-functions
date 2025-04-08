c577a43b1b1b9445cb7a32b5e2e3b842b0be7fab
lucioacri <lucioacri@gmail.com>
2025-04-08 14:56:22 +0200

    add names initials + arrow function

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,38 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
-const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+const names = [
+  "Anna",
+  "Luca",
+  "Marco",
+  "Adele",
+  "Giovanni",
+  "Alessandra",
+  "Carlo",
+  "Franco",
+];
 
 // Dichiara la funzione qui.
+// function getInitials(takeNames, initials) {
+//   const namesInitial = [];
+//   for (let i = 0; i < takeNames.length; i++) {
+//     let arrayNames = takeNames[i];
+//     let initials = arrayNames[0];
+//     namesInitial.push(initials);
+//   }
+//   return namesInitial;
+// }
 
-
-// Invoca la funzione qui e stampa il risultato in console
-
-
+// // Invoca la funzione qui e stampa il risultato in console
+// console.log(getInitials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+const getInitials = (takeNames, initials) => {
+  const namesInitial = [];
+  for (let i = 0; i < takeNames.length; i++) {
+    let arrayNames = takeNames[i];
+    let initials = arrayNames[0];
+    namesInitial.push(initials);
+  }
+  return namesInitial;
+};
+console.log(getInitials(names));
