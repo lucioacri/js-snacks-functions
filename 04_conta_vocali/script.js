de1dc635202ebe3be5946bccb4f41584be4a64dc
lucioacri <lucioacri@gmail.com>
2025-04-08 15:24:00 +0200

    add vowels

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,26 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
-
+function getVowels(takeWord) {
+  let vowelsNumber = 0;
+  for (let i = 0; i < takeWord.length; i++) {
+    let letters = takeWord[i];
+    if (
+      letters === "a" ||
+      letters === "e" ||
+      letters === "i" ||
+      letters === "o" ||
+      letters === "u"
+    ) {
+      vowelsNumber += 1;
+    }
+  }
+  return vowelsNumber;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(getVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
